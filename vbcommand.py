78481f3a8544b6646f2eee80a9d6aba93654c89c
Joe Schultz <jxscad@rit.edu>
2025-04-20 21:54:23 -0400

    Initialize main program script

---
+++ vbcommand.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#
+# Author: Joe Schultz <jxscad@rit.edu>
+# Date: 04/20/2025
+#
+# Description: Menu-driven program to control VirtualBox
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Main program
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
